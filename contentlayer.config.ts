d69adda2f38d4215539f3a1e7cceb2feff97fad4
hangyuCho <chg3376@gmail.com>
2023-07-11 00:24:16 +0900

    fix : bugfix for deploy

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,7 +3,7 @@ import { ComputedFields, defineDocumentType, makeSource } from 'contentlayer/sou
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
 import rehypePrettyCode from "rehype-pretty-code"
 import rehypeSlug from "rehype-slug"
-import remarkGfm from "remark-Gfm"
+import remarkGfm from "remark-gfm"
 
 const computedFields: ComputedFields = {
   slug: {
