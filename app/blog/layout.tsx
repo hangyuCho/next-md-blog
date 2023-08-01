662ad6badfd7085e38404a095729150faa98566e
hangyuCho <chg3376@gmail.com>
2023-08-02 08:50:11 +0900

    fix : add mdx and with mdx theme

---
+++ app/blog/layout.tsx
@@ -0,0 +1,5 @@
+const Layout = ( {children}: {children: React.ReactNode} ) => {
+    return <article className="prose prose-xl">{children}</article>
+}
+
+export default Layout
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,md,mdx}',
   ],
   theme: {
     extend: {
@@ -14,5 +14,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 }
